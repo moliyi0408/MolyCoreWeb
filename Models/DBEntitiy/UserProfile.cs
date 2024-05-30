4b386b6a996592ade5c6c8618c8cc687cfb38d3f
unknown <P1587@tsecpv.com>
2024-05-30 15:33:13 +0800

    db update

--- Models/DBEntitiy/UserProfile.cs
+++ Models/DBEntitiy/UserProfile.cs
@@ -4,8 +4,6 @@
     {
         public int ProfileId { get; set; }
         public int UserId { get; set; }
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string DateOfBirth { get; set; } = string.Empty;
 
